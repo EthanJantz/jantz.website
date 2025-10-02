c63b36d6f9290088360bcf29afea687d3dedb1d6
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2025-10-02 17:15:55 -0500

    design complete

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://ethanjantz.github.io",
+  base: "main",
   //site: 'https://ethanjantz.github.io',
   //base: 'jantz.website/www',
-})
+});
