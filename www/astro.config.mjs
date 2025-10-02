39ab7386437d7ff97dc4aa73ff882d013e5f9def
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2025-10-02 17:25:11 -0500

    update config for deployment

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -2,8 +2,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://ethanjantz.github.io",
-  base: "main",
-  //site: 'https://ethanjantz.github.io',
-  //base: 'jantz.website/www',
+  site: 'https://jantz.website',
 });
